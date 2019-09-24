72c01b018652ca8ea51b36e2d7d863a1fd71b3ca
maruf55 <38175651+maruf55@users.noreply.github.com>
2019-09-24 23:33:35 +0530

    Create truthTableXOR.c

---
+++ truthTableXOR.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+int main()
+{
+    int a[4],b[4],x,c[4];
+    printf("Enter four bits:");
+    for(x=0;x<4;x++){
+        scanf("%d",&a[x]);
+        if(a[x]>1 || a[x]<0){
+            x--;
+            continue;
+        }
+    }
+    printf("Enter four bits:");
+    for(x=0;x<4;x++){
+        scanf("%d",&b[x]);
+        if(b[x]>1 || b[x]<0){
+            x--;
+            continue;
+        }
+    }
+    printf("\nA\tB\tC");
+    for(x=0;x<4;x++){
+        if(a[x]==1 && b[x]==0)
+            c[x]=0;
+        else if(a[x]==0 && b[x]==1)
+            c[x]=0;
+        else
+            c[x]=1;
+        printf("\n%d\t%d\t%d",a[x],b[x],c[x]);
+    }
+    return 0;
+}
